355e7764d65d906ce93ac7ef422e77719e429e48
eljakj <jadjakhlab@gmail.com>
2026-04-14 01:22:26 +0200

    Update server file

--- client/src/services/todoApi.js
+++ client/src/services/todoApi.js
@@ -1,3 +1,5 @@
+console.log("TODO API URL:", `${import.meta.env.VITE_API_URL}/api/todos`);
+
 const API_BASE_URL = (
   import.meta.env.VITE_API_URL || "http://localhost:5000"
 ).replace(/\/$/, "");
