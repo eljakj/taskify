e77bf169db2f276cc473729d4f0b140a1b76a1db
eljakj <jadjakhlab@gmail.com>
2026-04-14 00:54:24 +0200

    Update server file

--- server/server.js
+++ server/server.js
@@ -15,8 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
   }),
 );
 app.use(express.json());
